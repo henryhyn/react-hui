e143c4cf58a495df9dae9df4918de5ef7322e3eb
何永能 <yongneng.he@dianping.com>
2016-10-25 16:47:00 +0800

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ var DIST_PATH = path.resolve(ROOT_PATH, 'dist');
 module.exports = {
     // 项目入口. 可以直接用文件夹名称, 默认会找 index.js; 也可以确定是哪个文件名字
     entry: {
-        app: SRC_PATH,
-        hui: [path.resolve(SRC_PATH, 'components')]
+        app: SRC_PATH
     },
     // 项目出口. 让 webpack 把处理完成的文件放在哪里
     output: {
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,11 @@
+var path = require('path');
 var webpack = require('webpack');
+// 定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var SRC_PATH = path.resolve(ROOT_PATH, 'src');
 
 var config = require('./webpack.config.js');
+config.entry.hui = [path.resolve(SRC_PATH, 'components')];
 config.plugins.push(
     new webpack.DefinePlugin({
         'process.env': {
