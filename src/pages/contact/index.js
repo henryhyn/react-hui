07e5cdd0b0d30d523ebdda7a35785f079e7b8903
何永能 <yongneng.he@dianping.com>
2016-10-27 18:03:40 +0800

    modify

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 import DocumentTitle from 'react-document-title';
+import { SearchBox, Hex } from '../../components';
+import { Icon } from 'antd';
 
 class Contact extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            total: 0,
+            data: [],
+            completed: false,
+            visible: false
+        };
+    }
+
+    handleSearch(keyword) {
+        Hex.get('https://api.github.com/search/users', {q: keyword}, data => this.setState({
+            total: data.total_count,
+            data: data.items,
+            completed: data.incomplete_results
+        }));
+    }
+
+    renderData(data) {
+        if (data === null || data === undefined || data.length === 0) {
+            return false;
+        }
+        return (
+            <div>
+                {
+                    data.map((item, i)=> {
+                        return <div key={i}>{item.login}</div>;
+                    })
+                }
+            </div>
+        );
     }
 
     render() {
         return (
             <DocumentTitle title='联系我们'>
                 <div className='main-wrapper'>
-                    <div className='wrapper'>contact</div>
+                    <div className='wrapper'>
+                        <Icon type='link'/>
+                        <SearchBox onSearch={this.handleSearch.bind(this)}/>
+                        {this.renderData(this.state.data)}
+                    </div>
                 </div>
             </DocumentTitle>
         );
